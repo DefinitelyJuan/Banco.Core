fe1456b524e72975c8c1b71be0718aa1211d2a0a
zolanovich <avilajuan0301@gmail.com>
2022-04-14 23:32:38 -0400

    Seguridad mejorada haciendo uso de ConfigurationManager

--- CoreAplicacion/CapaServicio/Autenticacion.cs
+++ CoreAplicacion/CapaServicio/Autenticacion.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Configuration;
+
 namespace CoreAplicacion.CapaServicio
 {
     public class Autenticacion
